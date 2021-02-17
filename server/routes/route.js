900b6e7ed9ed66dae60e2b5fa7860944f2ccbb2b
tranttxuan <>
2021-02-17 15:03:16 +0100

    edit/delete/search functions

--- server/routes/route.js
+++ server/routes/route.js
@@ -5,7 +5,7 @@ const db = require("../dbService")
 //get all data
 router.get("/", (req, res) => {
   let sqlQuery = "SELECT * FROM names";
-  db.query(sqlQuery, (err, result)=>{
+  db.query(sqlQuery, (err, result) => {
     if (err) res.status(500).json({ message: err });
     console.log(result)
     res.status(200).json(result)
@@ -23,9 +23,29 @@ router.post('/create', (req, res, next) => {
     // console.log(result.insertId)
     res.status(200).json(result)
   })
- 
+
 });
 
+//edit
+router.patch("/:id", (req, res) => {
+  console.log("check req.body", req.body)
+  let sqlQuery = "UPDATE names SET name=? WHERE id=?;";
+  db.query(sqlQuery, [req.body.name, req.params.id], (err, result) => {
+    if (err) res.status(500).json({ message: err });
+    console.log("resul>>", result)
+    res.status(200).json(result)
+  })
+})
+
+//delete
+router.delete("/:id", (req, res) => {
+  let sqlQuery = "DELETE FROM names WHERE id=?"
+  db.query(sqlQuery, req.params.id, (err, result) => {
+    if (err) res.status(500).json({ message: err });
+    console.log(result)
+    res.status(200).json(result)
+  })
+})
 
 
 module.exports = router;
